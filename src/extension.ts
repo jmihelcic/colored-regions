ea3bebd422fb84036fe321559609eb22746d1442
Kasheftin <Kasheftin@gmail.com>
2024-03-01 11:52:54 +0200

    split screen support added, cache issue in multiple opened editors fixed

--- src/extension.ts
+++ src/extension.ts
@@ -4,30 +4,46 @@ import ColoredRegions, { DecoratorMap } from './ColoredRegions'
 
 export const activate = (context: vscode.ExtensionContext) => {
   const coloredRegions = new ColoredRegions()
-  let decoratorMap: DecoratorMap = {}
+  const decoratorMaps: Record<string, DecoratorMap> = {}
 
-  const updateColoredRegions = (editor: vscode.TextEditor) => {
+  const updateColoredRegions = (editor: vscode.TextEditor, editorId: number) => {
     const newDecoratorMap = coloredRegions.getDecoratorMap(editor.document.getText())
-
-    Object.entries(decoratorMap).forEach(([color, { decorator }]) => {
+    const key = editorId + '|' + editor.document.uri.fsPath
+    if (!decoratorMaps[key]) {
+      decoratorMaps[key] = {}
+    }
+    Object.entries(decoratorMaps[key]).forEach(([color, { decorator }]) => {
       if (!newDecoratorMap[color]) {
         editor.setDecorations(decorator, [])
       }
     })
     Object.entries(newDecoratorMap).forEach(([color, { decorator, regions, regionsKey }]) => {
-      if (decoratorMap[color]?.regionsKey !== regionsKey) {
+      if (decoratorMaps[key][color]?.regionsKey !== regionsKey) {
         editor.setDecorations(decorator, regions)
       }
     })
-    decoratorMap = newDecoratorMap
+    decoratorMaps[key] = newDecoratorMap
+  }
+
+  const cleanupClosedEditors = () => {
+    const openedEditorByKeys = vscode.window.visibleTextEditors.reduce((out: Record<string, true>, editor, editorId) => {
+      out[editorId + '|' + editor.document.uri.fsPath] = true
+      return out
+    }, {})
+    Object.keys(decoratorMaps).forEach((key) => {
+      if (!openedEditorByKeys[key]) {
+        delete decoratorMaps[key]
+      }
+    })
   }
 
   const readConfiguration = async () => {
     const configuration = await getConfiguration()
     coloredRegions.setConfiguration(configuration)
-    vscode.window.visibleTextEditors.map((editor) => {
-      updateColoredRegions(editor)
+    vscode.window.visibleTextEditors.forEach((editor, editorId) => {
+      updateColoredRegions(editor, editorId)
     })
+    cleanupClosedEditors()
   }
 
   vscode.workspace.onDidChangeConfiguration((event) => {
@@ -36,16 +52,24 @@ export const activate = (context: vscode.ExtensionContext) => {
     }
   }, null, context.subscriptions)
 
-  vscode.window.onDidChangeActiveTextEditor((editor) => {
-    if (editor) {
-      updateColoredRegions(editor)
-    }
+  vscode.window.onDidChangeVisibleTextEditors((editors) => {
+    const documentsChanged = editors.reduce((out: Record<string, true>, editor) => {
+      out[editor.document.uri.fsPath] = true
+      return out
+    }, {})
+    vscode.window.visibleTextEditors.forEach((editor, editorId) => {
+      if (documentsChanged[editor.document.uri.fsPath]) {
+        updateColoredRegions(editor, editorId)
+      }
+    })
+    cleanupClosedEditors()
   }, null, context.subscriptions)
 
   vscode.workspace.onDidChangeTextDocument((event) => {
-    vscode.window.visibleTextEditors.map((editor) => {
-      if (editor.document === event.document) {
-        updateColoredRegions(editor)
+    vscode.window.visibleTextEditors.forEach((editor, editorId) => {
+      if (editor.document.uri.fsPath === event.document.uri.fsPath) {
+        updateColoredRegions(editor, editorId)
+        return true
       }
     })
   }, null, context.subscriptions)
